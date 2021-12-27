e83aa2ec9d3817a23201ad9492f2825982e498fc
Luke Rucker <ruckerlh@gmail.com>
2021-12-27 13:37:19 -0600

    add hero section

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,5 +1,12 @@
 import * as React from 'react'
 
 export default function Index() {
-  return <div>hello world</div>
+  return (
+    <div className="text-2xl font-bold">
+      <h2>Hello world, I&apos;m Luke Rucker.</h2>
+      <p className="text-gray-500">
+        I name things. Oh, and sometimes I build software.
+      </p>
+    </div>
+  )
 }
