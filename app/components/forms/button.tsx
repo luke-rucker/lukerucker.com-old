2294487ecf614f07f6595c7b544dfa711089be60
Luke Rucker <ruckerlh@gmail.com>
2022-01-04 11:46:02 -0600

    fix button focus styling, add link variant to button component

--- app/components/forms/button.tsx
+++ app/components/forms/button.tsx
@@ -1,8 +1,8 @@
 import clsx from 'clsx'
 import * as React from 'react'
 
-type ButtonProps = {
-  variant?: 'primary' | 'danger'
+export type ButtonProps = {
+  variant?: 'primary' | 'danger' | 'link'
   children: React.ReactNode
 } & React.DetailedHTMLProps<
   React.ButtonHTMLAttributes<HTMLButtonElement>,
@@ -19,13 +19,15 @@ export function Button({
   return (
     <button
       className={clsx(
-        'px-3 py-2 focus:ring-2',
-        {
-          'bg-gray-200 hover:bg-gray-300 focus:ring-gray-500':
-            variant === 'primary',
-          'bg-red-200 text-red-600 hover:text-red-800 hover:bg-red-300 focus:ring-red-500':
-            variant === 'danger',
-        },
+        variant === 'link'
+          ? 'link'
+          : [
+              'px-3 py-2 focus:outline-none border-2 border-transparent',
+              variant === 'primary' &&
+                'bg-gray-200 hover:bg-gray-300 hover:border-gray-300 focus:border-gray-500',
+              variant === 'danger' &&
+                'bg-red-200 text-red-600 hover:text-red-800 hover:bg-red-300 hover:border-red-300 focus:border-red-500',
+            ],
         className
       )}
       {...props}
